690d4662c335ca0b377762eead88f674535767e0
sword36 <sword36rus@gmail.com>
2015-10-20 20:31:45 +0300

    Big change in REST api and in entities models

--- app.js
+++ app.js
@@ -84,199 +84,245 @@ var CollisionSchema = mongoose.Schema({
 var GameSchema = mongoose.Schema({
   start: Date,
   end: Date,
-  playerName: String,
-  scores: Number,
   path: [PointSchema],
   collisions: [CollisionSchema],
   breatheAmount: Number,
-  viewPort: [Number]
+  viewPort: [Number],
+
+  playerId: ObjectId,
+  scores: Number
 });
 
 var SessionSchema = mongoose.Schema({
   start: Date,
   end: Date,
-  games: [GameSchema],
-  hostComputer: String
+  hostComputer: String,
+
+  gamesId: [ObjectId]
 });
 
-var RecordSchema = mongoose.Schema({
+var MentorSchema = mongoose.Schema({
+  name: String,
+  surname: String,
+  login: String,
+  password: Number,
+
+  playersId: [ObjectId]
+});
+
+var PlayerSchema = mongoose.Schema({
   name: String,
   scores: Number,
   hostComputer: String,
   place: Number
 });
 
-var RecordModel = mongoose.model("Record", RecordSchema);
+var PlayerModel = mongoose.model("Record", PlayerSchema);
 var SessionModel = mongoose.model("Session", SessionSchema);
+var GameModel = mongoose.model("Game", GameSchema);
+var MentorModel = mongoose.model("Mentor", MentorSchema);
 
-//records router
-app.get("/api/records", function(request, responce) {
-  return RecordModel.find(function(err, records) {
-    if (!err) {
-      console.log("Records read");
-      return responce.send(records);
-    } else {
-      return console.log(err);
-    }
-  })
-});
-
-app.get("/api/records/:id", function(request, responce) {
-  return RecordModel.findById(request.params.id, function(err, record) {
-    if (!err) {
-      console.log("Record read");
-      return responce.send(record);
-    } else {
-      return console.log(err);
-    }
-  });
-});
-
-app.post("/api/records", function(request, responce) {
-  var record = new RecordModel({
-    name: request.body.name,
-    scores: request.body.scores,
-    hostComputer: request.body.hostComputer
-  });
-
-  record.save(function(err) {
-    if (!err) {
-      console.log("Record created");
-      return responce.send(record);
-    } else {
-      console.log(err);
-    }
-  })
-});
-
-app.put("/api/records/:id", function(request, responce) {
-  return RecordModel.findById(request.params.id, function(err, record) {
-    if (!err) {
-      record.name = request.body.name;
-      record.scores = request.body.scores;
-      record.hostComputer = request.body.hostComputer;
-      record.place = request.body.place;
-
-      record.save(function(err) {
-        if (!err) {
-          console.log("Record updated");
-          return responce.send(record);
-        } else {
-          console.log(err);
-        }
-      })
-    }
-  });
-});
-
-app.delete("/api/records/:id", function(request, responce) {
-  return RecordModel.findById(request.params.id, function(err, record) {
-    return record.remove(function(err) {
-      if (!err) {
-        console.log("Record removed");
-        responce.send("");
-      } else {
-        console.log(err);
-      }
-    })
-  })
-});
-
-
-//sessions route
-app.get("/api/sessions", function(request, responce) {
-  return SessionModel.find(function(err, sessions) {
-    if (!err) {
-      console.log("Sessions read");
-      return responce.send(sessions);
-    } else {
-      return console.log(err);
-    }
-  })
-});
-
-app.get("/api/sessions/:id", function(request, responce) {
-  return SessionModel.findById(request.params.id, function(err, session) {
-    if (!err) {
-      console.log("Session read");
-      return responce.send(session);
-    } else {
-      return console.log(err);
-    }
-  });
-});
-
-app.post("/api/sessions", function(request, responce) {
-  if (!Array.isArray(request.body)) {
-    var session = new SessionModel({
-      start: request.body.start,
-      end: request.body.end,
-      hostComputer: request.body.hostComputer,
-      games: request.body.games
-    });
-
-    session.save(function(err) {
-      if (!err) {
-        console.log("Session created");
-        return responce.send("created");
-      } else {
-        console.log(err);
-      }
-    });
-  } else {
-    //var sessions = [];
-    request.body.forEach(function(sessionBody) {
-      var session = new SessionModel({
-        start: sessionBody.start,
-        end: sessionBody.end,
-        hostComputer: sessionBody.hostComputer,
-        games: sessionBody.games
-      });
-
-      session.save(function(err) {
-        if (!err) {
-          console.log("Session created");
-          //sessions.push(JSON.parse(JSON.stringify(session)));
-        } else {
-          console.log(err);
-        }
-      });
-    });
-    return responce.send("created");
+var setModelFromBody = function(body, model) {
+  var obj;
+  if (typeof model == "undefined") { //then create model, else update old(not necessary all keys)
+    obj = Object.create(null);
   }
-});
 
-app.put("/api/sessions/:id"                                                                                                                                                                                                                                                                                                                                                                                               , function(request, responce) {
-  return SessionModel.findById(request.params.id, function(err, session) {
-    if (!err) {
-      session.start = request.body.start;
-      session.end = request.body.end;
-      session.hostComputer = request.body.hostComputer;
-      session.games = request.body.games;
+  for (var attr in body) {
+    if (body.hasOwnProperty(attr)) {
+      obj[attr] = body[attr];
+    }
+  }
 
-      session.save(function(err) {
+  return obj;
+};
+
+function createRestFromEntities(entities) {
+  entities.forEach(function(ent) {
+    var ents = ent + "s";
+
+    var Model;
+    switch (ent) {
+      case "session":
+            Model = SessionModel;
+            break;
+      case "game":
+            Model = GameModel;
+            break;
+      case "player":
+            Model = PlayerModel;
+            break;
+      case "mentor":
+            Model = MentorModel;
+            break;
+      default : throw new Error("Wrong entity");
+    }
+
+    app.get("/api/" + ents, function(request, responce) {
+      return Model.find(function(err, models) {
         if (!err) {
-          console.log("Session updated");
-          return responce.send(session);
+          console.log(ents + " read");
+          return responce.send(models);
+        } else {
+          return console.log(err);
+        }
+      })
+    });
+
+    app.get("/api/" + ents + "/:id", function(request, responce) {
+      return Model.findById(request.params.id, function(err, model) {
+        if (!err) {
+          console.log(ent + " read");
+          return responce.send(model);
+        } else {
+          return console.log(err);
+        }
+      });
+    });
+
+    app.post("/api/" + ents, function(request, responce) {
+      var model = new Model(setModelFromBody(request.body));
+
+      model.save(function(err) {
+        if (!err) {
+          console.log(ent + " created");
+          return responce.send(model);
         } else {
           console.log(err);
         }
       })
-    }
-  });
-});
+    });
 
-app.delete("/api/sessions/:id", function(request, responce) {
-  return SessionModel.findById(request.params.id, function(err, session) {
-    return session.remove(function(err) {
-      if (!err) {
-        console.log("Session removed");
-        responce.send("");
-      } else {
-        console.log(err);
-      }
-    })
-  })
-});
+    app.put("/api/" + ents + "/:id", function(request, responce) {
+      return Model.findById(request.params.id, function(err, model) {
+        if (!err) {
+          setModelFromBody(request.body, model);
+
+          model.save(function(err) {
+            if (!err) {
+              console.log(ent + " updated");
+              return responce.send(model);
+            } else {
+              console.log(err);
+            }
+          })
+        }
+      });
+    });
+
+    app.delete("/api/" + ents + "/:id", function(request, responce) {
+      return Model.findById(request.params.id, function(err, model) {
+        return model.remove(function(err) {
+          if (!err) {
+            console.log(ent + " removed");
+            responce.send("");
+          } else {
+            console.log(err);
+          }
+        })
+      })
+    });
+  }, this);
+}
+
+var entities = ["session", "game", "player", "mentor"];
+
+createRestFromEntities(entities);
+
+////sessions route
+//app.get("/api/sessions", function(request, responce) {
+//  return SessionModel.find(function(err, sessions) {
+//    if (!err) {
+//      console.log("Sessions read");
+//      return responce.send(sessions);
+//    } else {
+//      return console.log(err);
+//    }
+//  })
+//});
+//
+//app.get("/api/sessions/:id", function(request, responce) {
+//  return SessionModel.findById(request.params.id, function(err, session) {
+//    if (!err) {
+//      console.log("Session read");
+//      return responce.send(session);
+//    } else {
+//      return console.log(err);
+//    }
+//  });
+//});
+//
+//app.post("/api/sessions", function(request, responce) {
+//  if (!Array.isArray(request.body)) {
+//    var session = new SessionModel({
+//      start: request.body.start,
+//      end: request.body.end,
+//      hostComputer: request.body.hostComputer,
+//      games: request.body.games
+//    });
+//
+//    session.save(function(err) {
+//      if (!err) {
+//        console.log("Session created");
+//        return responce.send("created");
+//      } else {
+//        console.log(err);
+//      }
+//    });
+//  } else {
+//    //var sessions = [];
+//    request.body.forEach(function(sessionBody) {
+//      var session = new SessionModel({
+//        start: sessionBody.start,
+//        end: sessionBody.end,
+//        hostComputer: sessionBody.hostComputer,
+//        games: sessionBody.games
+//      });
+//
+//      session.save(function(err) {
+//        if (!err) {
+//          console.log("Session created");
+//          //sessions.push(JSON.parse(JSON.stringify(session)));
+//        } else {
+//          console.log(err);
+//        }
+//      });
+//    });
+//    return responce.send("created");
+//  }
+//});
+//
+//app.put("/api/sessions/:id"                                                                                                                                                                                                                                                                                                                                                                                               , function(request, responce) {
+//  return SessionModel.findById(request.params.id, function(err, session) {
+//    if (!err) {
+//      session.start = request.body.start;
+//      session.end = request.body.end;
+//      session.hostComputer = request.body.hostComputer;
+//      session.games = request.body.games;
+//
+//      session.save(function(err) {
+//        if (!err) {
+//          console.log("Session updated");
+//          return responce.send(session);
+//        } else {
+//          console.log(err);
+//        }
+//      })
+//    }
+//  });
+//});
+//
+//app.delete("/api/sessions/:id", function(request, responce) {
+//  return SessionModel.findById(request.params.id, function(err, session) {
+//    return session.remove(function(err) {
+//      if (!err) {
+//        console.log("Session removed");
+//        responce.send("");
+//      } else {
+//        console.log(err);
+//      }
+//    })
+//  })
+//});
 
 module.exports = app;
