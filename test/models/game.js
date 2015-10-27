619d92bad1ff33609a0b1b3eedd211f4c1883116
sword36 <sword36rus@gmail.com>
2015-10-27 19:31:01 +0300

    Game model tested

--- test/models/game.js
+++ test/models/game.js
@@ -31,44 +31,58 @@ describe("Game model", function() {
             position: [10, 20]
         }],
         breatheAmount: 1000,
-        viewPort: [1000, 1000],
+        viewPort: [1000, 1200],
         _player: player._id,
         scores: 100
     };
 
-    var PointSchema = mongoose.Schema({
-        p: [Number]
-    }, {_id: false});
-
-    var PointModel = mongoose.model("Point", PointSchema);
-
     it("can create a new Game", function(done) {
-
         GameModel.create(game, function(err, model) {
-
             expect(err).to.not.exist;
             expect(model.start).to.deep.equal(new Date(1000));
             expect(model.end).to.deep.equal(new Date(2000));
 
-            //var p1 = new PointModel({p: [1, 1]});
-            //var p2 = new PointModel({p: [2, 3]});
-
             expect(model.path[0].p[0]).to.equal(1);
+            expect(model.path[0].p[1]).to.equal(1);
             expect(model.path[1].p[0]).to.equal(2);
             expect(model.path[1].p[1]).to.equal(3);
 
-            /*
-            expect(model.collisions).to.deep.equal([{
-                class: "bonus",
-                type: "big",
-                position: [10, 20]
-            }]);*/
+
+            expect(model.collisions[0].class).to.equal("bonus");
+            expect(model.collisions[0].type).to.equal("big");
+            expect(model.collisions[0].position[0]).to.equal(10);
+            expect(model.collisions[0].position[1]).to.equal(20);
+
             expect(model.breatheAmount).to.equal(1000);
-            //expect(model.viewPort).to.equal([ 1000, 1000 ]);
-            //expect(model._id).to.equal(player._id);
+            expect(model.viewPort[0]).to.equal(1000);
+            expect(model.viewPort[1]).to.equal(1200);
+
             expect(model.scores).to.equal(100);
+            expect(model._player).to.equal(player._id);
 
             done();
         });
+    });
+
+    it("can be populated by Player", function(done) {
+        player.save()
+            .then(function(p) {
+                return GameModel.create(game);
+            })
+            .then(function(g) {
+                return GameModel.findOne({_id: g._id}).populate("_player").exec();
+            })
+            .then(function(g) {
+                expect(g._player).to.be.ok;
+                expect(g._player._id).to.be.deep.equal(player._id);
+                expect(g._player.name).to.be.equal("Oleg");
+
+                done();
+            })
+            .then(undefined, function(err) { //catch(..) {}
+                expect(err).to.not.exist;
+
+                done();
+            })
     });
 });
