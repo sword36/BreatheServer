4cdd2beb94a695b7c463d8a2acda9da94ba6120d
sword36 <sword36rus@gmail.com>
2015-10-31 22:54:40 +0300

    travis

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        "development": "mongodb://127.0.0.1:27017",
+        "development": "mongodb://127.0.0.1:27018",
         "production": "mongodb://" + process.env.dbuser + ":" + process.env.dbpassword
         + "@ds041643.mongolab.com:41643/breatheserver",
         "test": "mongodb://127.0.0.1:27017",
