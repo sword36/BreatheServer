4275c87a3439ab3fac67a8c5102aab4bdcd3d944
sword36 <sword36rus@gmail.com>
2015-10-31 20:42:37 +0300

    package

--- test/support/utils.js
+++ test/support/utils.js
@@ -1,19 +1,22 @@
 var config = require("../../config");
 var mongoose = require("mongoose");
 
+
 beforeEach(function(done) {
     function clearDB() {
-        for (var i in mongoose.connection.collections) {
+        for (var i in mongoose.connection.collections)  {
             if (mongoose.connection.collections.hasOwnProperty(i)) {
                 mongoose.connection.collections[i].remove({}, function() {});
             }
         }
+
         return done();
     }
 
     if (mongoose.connection.readyState === 0) {
         mongoose.connect(config.db.test, function(err) {
             if (err) {
+                console.log("err");
                 throw  err;
             }
             return clearDB();
