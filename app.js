3ea8e278abe92b9b4aac8b265353724abc6cfcdd
sword36 <sword36rus@gmail.com>
2015-10-31 20:08:44 +0300

    travis

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var Promise = require('promise');
+//var Promise = require('promise');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
         "development": "mongodb://localhost:27017",
         "production": "mongodb://" + process.env.dbuser + ":" + process.env.dbpassword
         + "@ds041643.mongolab.com:41643/breatheserver",
-        "test": "mongodb://localhost:27018"
+        "test": "mongodb://localhost:27018",
+        "travis": "mongodb://127.0.0.1"
     }
 };
