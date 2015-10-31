ab1bd037ebe2c262da5135d37cb70d1837722cd8
sword36 <sword36rus@gmail.com>
2015-10-31 21:12:27 +0300

    travis

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
         "production": "mongodb://" + process.env.dbuser + ":" + process.env.dbpassword
         + "@ds041643.mongolab.com:41643/breatheserver",
         "test": "mongodb://localhost:27018",
-        "travis": "mongodb://127.0.0.1"
+        "travis": "mongodb://127.0.0.1:27017"
     }
 };
