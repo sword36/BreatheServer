d4b869fd9c33be120fb0d751e97188486fde796d
sword36 <sword36rus@gmail.com>
2015-11-02 18:36:14 +0300

    Environment for Backbone with Backgrid.js and Backbone.paginator

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
 /**
  * Created by USER on 31.10.2015.
  */
+global.$ = require("jQuery");
+global._ = require("underscore");
+global.Backbone = require("Backbone");
+global.Backbone.PageableCollection = require("backbone.paginator");
